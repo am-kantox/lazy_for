2b6882170bf9f2c3ef346aec595cb1cbe7850a36
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-10-01 09:13:48 +0200

    My own formatter impl.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,52 @@
+defmodule LazyFor.CLIFormatter do
+  @moduledoc false
+  use GenServer
+
+  import ExUnit.Formatter,
+    only: [format_time: 2, format_filters: 2, format_test_failure: 5, format_test_all_failure: 5]
+
+  ## Callbacks
+
+  def init(opts) do
+    config = %{
+      seed: opts[:seed],
+      trace: opts[:trace],
+      colors: Keyword.put_new(opts[:colors], :enabled, IO.ANSI.enabled?()),
+      width: get_terminal_width(),
+      slowest: opts[:slowest],
+      test_counter: %{},
+      test_timings: [],
+      failure_counter: 0,
+      skipped_counter: 0,
+      excluded_counter: 0,
+      invalid_counter: 0
+    }
+
+    {:ok, config}
+  end
+
+  def handle_cast({:suite_started, _opts}, config) do
+    IO.puts("Started")
+    {:noreply, config}
+  end
+
+  def handle_cast({:suite_finished, run_us, load_us}, config) do
+    IO.inspect({{:suite_finished, run_us, load_us}, config}, label: "Finished")
+    {:noreply, config}
+  end
+
+  def handle_cast(_, config) do
+    {:noreply, config}
+  end
+
+  defp get_terminal_width do
+    case :io.columns() do
+      {:ok, width} -> max(40, width)
+      _ -> 80
+    end
+  end
+end
+
+ExUnit.configure(formatters: [ExUnit.CLIFormatter, LazyFor.CLIFormatter])
+
 ExUnit.start()
